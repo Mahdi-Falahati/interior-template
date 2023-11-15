ece8781c1c7b9d31a70c554425c17d801aaf7810
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-11-15 01:26:39 -0800

    make route for services page

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import {
 import RootLayouts from "./layouts/RootLayouts";
 import HomePages from "./Pages/HomePages";
 import AboutUs from "./Pages/AboutUs";
+import Services from "./Pages/Services";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayouts />}>
       <Route index element={<HomePages />} />
       <Route path="/about-us" element={<AboutUs />} />
+      <Route path="/services" element={<Services />} />
     </Route>
   )
 );

c6351ffebb502384c06c473f6ef63aaff5f79101
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-11-15 05:38:48 -0800

    complete pricing page

--- src/Pages/Pricing.jsx
+++ src/Pages/Pricing.jsx
@@ -1,5 +1,71 @@
+import { Container, Stack } from "@mui/material";
+
+import MainHeder from "../components/MainHeder";
+import PricingCard from "../components/PricingCard";
+
+import banner from "../assets/pricing/banner.jpg";
+
 export default function Pricing() {
   return (
-    <div>Pricing</div>
-  )
+    <>
+      <MainHeder banner={banner} />
+      <Container>
+        <Stack
+          flexDirection="row"
+          justifyContent="space-around"
+          flexWrap="wrap"
+          my={6}
+        >
+          {Prices?.map(({ bestPrice, title, price, info }, id) => (
+            <PricingCard
+              bestPrice={bestPrice}
+              title={title}
+              price={price}
+              info={info}
+              key={id}
+            />
+          ))}
+        </Stack>
+      </Container>
+    </>
+  );
 }
+
+const Prices = [
+  {
+    bestPrice: false,
+    title: "Design advices",
+    price: 29,
+    info: [
+      "General living space advices",
+      "Rennovation advices",
+      "Interior design advices",
+      "Furniture reorganization",
+      "Up to 5 hours meetings",
+    ],
+  },
+  {
+    bestPrice: true,
+    title: "Complete interior",
+    price: 39,
+    info: [
+      "Complete home redesign",
+      "Interior and exterior works",
+      "Modular interior planning",
+      "Kitchen design",
+      "Garages organization",
+    ],
+  },
+  {
+    bestPrice: false,
+    title: "Furniture design",
+    price: 59,
+    info: [
+      "Furniture for living room",
+      "Furniture refurbishment",
+      "Sofas and amchairs",
+      "Tables and chairs",
+      "Kitchens",
+    ],
+  },
+];

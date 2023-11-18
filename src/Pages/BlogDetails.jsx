b41ffa86b19e763508c74305298ff802ee70713d
Mahdi-Falahati <75115762+Mahdi-Falahati@users.noreply.github.com>
2023-11-18 04:29:03 -0800

    addd main header to blogDetails page

---
+++ src/Pages/BlogDetails.jsx
@@ -0,0 +1,11 @@
+import MainHeader from '../components/MainHeader';
+
+import banner from '../assets/blogDetails/banner.jpg';
+
+export default function BlogDetails() {
+  return (
+    <>
+        <MainHeader Show={false} banner={banner}/>
+    </>
+    )
+}
